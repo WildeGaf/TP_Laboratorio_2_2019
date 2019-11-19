4c566440e34d5f73b5ad573bbf4fac17ded7d887
WildeGaf <WildeGaf>
2019-11-19 08:26:16 -0300

    Se inicia el tp4

---
+++ TP4_FloresGonzalo_2019/Entidades/Entidades/Correo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades
+{
+    public class Correo
+    {
+        private List<Thread> mockPaquetes;
+        private List<Paquete> paquetes;
+     
+        public List<Paquete> Paquetes
+        {
+            get { return paquetes; }
+            set { paquetes = value; }
+        }
+
+        public Correo() { }
+
+        public void FinEntregas()
+        {
+
+        }
+
+        public string MostrarDatos(Mostrar<List<Paquete>> elementos) { }
+
+        public static Correo operator *(Correo c, Paquete p) { }
+
+    }
+}
---
+++ TP4_FloresGonzalo_2019/Entidades/Entidades/PaqueteDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+
+namespace Entidades
+{
+    public class PaqueteDAO
+    {
+        private SqlCommand comando;
+        private SqlConnection conexion;
+
+        public bool Insertar(Paquete p) { }
+
+        private PaqueteDAO()
+        {
+
+        }
+    }
+}
